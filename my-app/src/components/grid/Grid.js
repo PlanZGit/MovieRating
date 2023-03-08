606f49b0a945f7db39f9d1e844d66bd486e10396
Vang Xiong <vangx709@gmail.com>
2023-03-07 20:39:37 -0900

    fix naming

--- my-app/src/components/grid/Grid.js
+++ my-app/src/components/grid/Grid.js
@@ -7,10 +7,10 @@ function Grid() {
   // console.log(movies);
 
   return (
-    <div className="grid">
+    <div className="flex">
       {movies.map((obj) => {
         return (
-          <div className="grid-container" key={obj.id}>
+          <div className="flex-container" key={obj.id}>
             <img className="image" alt={obj.title} src={obj.cover}></img>
             <div className="title">{obj.title.toUpperCase()}</div>
             <div className="rate">{obj.rate}/10</div>
