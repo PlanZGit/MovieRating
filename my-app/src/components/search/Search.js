78693d622aa80b304c3afdc95faeda3e7ffc0256
Vang Xiong <vangx709@gmail.com>
2023-03-09 19:49:01 -0900

    search bar in progress

--- my-app/src/components/search/Search.js
+++ my-app/src/components/search/Search.js
@@ -1,33 +1,40 @@
-import React, { useContext, useRef } from "react";
+import React, { useContext, useState } from "react";
 import "./Search.css";
 import { GoSearch } from "react-icons/go";
 import { MovieContext } from "../API/DataAxiosGet";
 
 function Search() {
-  const inputRef = useRef(null);
   const movies = useContext(MovieContext);
-  // console.log(movies);
+  const [filterMovies, setFilterMovies] = useState([]);
 
-  let filterMovie = [];
-  const handleSerach = (e) => {
-    filterMovie = movies.filter((obj) =>
+  const handleChange = (e) => {
+    //diplay block if input length > 0, else call handleBlur
+    if (e.target.value.length > 0) {
+      let dropDown = document.getElementById("search-dropdown-movie");
+      dropDown.style.display = "block";
+    } else {
+      handleBlur(e);
+    }
+
+    //filter search , grab only top 5
+    let filterArray = movies.filter((obj) =>
       obj.titleText["text"].toLowerCase().includes(e.target.value.toLowerCase())
     );
-    console.log(filterMovie);
+    filterArray.length = 5;
+
+    //set results
+    setFilterMovies(filterArray);
   };
 
-  const handleFocus = () => {
-    console.log("focus");
-    let dropDown = document.getElementById("search-dropdown-movie");
-    dropDown.style.display = "block";
-  };
-
-  const handleBlur = () => {
-    console.log("blur");
+  const handleBlur = (e) => {
+    //clear input value, clear filterMovies, display none
+    e.target.value = "";
     let dropDown = document.getElementById("search-dropdown-movie");
     dropDown.style.display = "none";
+    setFilterMovies([]);
   };
 
+  //TODO
   // Need to add a page filter
   // MovieReview/search/filter="sup"
   return (
@@ -35,42 +42,46 @@ function Search() {
       <div className="search-container">
         <input
           placeholder="Search"
-          ref={inputRef}
-          onChange={(e) => handleSerach(e)}
-          onFocus={() => {
-            handleFocus();
-          }}
-          onBlur={() => {
-            handleBlur();
+          onChange={(e) => handleChange(e)}
+          onBlur={(e) => {
+            handleBlur(e);
           }}
         />
+
+        {/* icon */}
         <GoSearch />
 
         <div className="search-dropdown-movie" id="search-dropdown-movie">
-          {/* {filterMovie.map{}} */}
           <div className="search-parent-container">
-            <div className="child-container">
-              <div className="search-dropdown-movie-img"></div>
-              <div className="search-dropdown-movie-details"></div>
-            </div>
-            <div className="child-container">
-              <div className="search-dropdown-movie-img"></div>
-              <div className="search-dropdown-movie-details"></div>
-            </div>
-            <div className="child-container">
-              <div className="search-dropdown-movie-img"></div>
-              <div className="search-dropdown-movie-details"></div>
-            </div>
-            <div className="child-container">
-              <div className="search-dropdown-movie-img"></div>
-              <div className="search-dropdown-movie-details"></div>
-            </div>
-            <div className="child-container">
-              <div className="search-dropdown-movie-img"></div>
-              <div className="search-dropdown-movie-details"></div>
-            </div>
+            {filterMovies.map((obj) => {
+              return (
+                <div className="child-container" key={obj.id}>
+                  <img
+                    src={obj.primaryImage["url"]}
+                    alt="img"
+                    className="search-dropdown-movie-img"
+                  ></img>
 
-            <a className="view-all">View all Results</a>
+                  <div className="search-dropdown-movie-details">Details</div>
+                </div>
+              );
+            })}
+
+            {/* 
+            <div className="child-container">
+              <div className="search-dropdown-movie-img">IMG</div>
+              <div className="search-dropdown-movie-details">
+                <p>
+                  Test as asd asd asd asd asd <br /> asd a sd as d asd a sd
+                  asdal
+                </p>
+              </div>
+            </div> */}
+
+            {/* if more then 5 */}
+            {/* <a href="#" className="view-all">
+              View all Results
+            </a> */}
           </div>
         </div>
       </div>
